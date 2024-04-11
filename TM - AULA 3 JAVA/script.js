0245dcefb346a9a28acc53a4a41bc8b3fa81f7fc
taismaia <148501573+taismaia@users.noreply.github.com>
2024-04-11 18:26:46 -0300

    Add files via upload

---
+++ TM - AULA 3 JAVA/script.js
@@ -0,0 +1,66 @@
+// const num1 = Number(prompt("Digite um número: "))
+// const num2 = Number(prompt("Digite um número: "))
+// const num3 = Number(prompt("Digite um número: "))
+
+// if(numero1 > numero2 && numero1 > numero3){
+//     alert(`O número ${numero1} é o maior`)
+// }else if(numero2 > numero1 && numero2 > numero3){
+//     alert(`O número ${numero2} é o maior`)
+// }else if(numero3 > numero1 && numero3 > numero2){
+//     alert(`O número ${numero3} é o maior`)
+// }else{
+//     alert(`Todos os números são iguais`)
+// }
+
+
+// const email = prompt("Digite seu email: ")
+// const senha = prompt("Digite sua senha: ")
+
+// const email_correto = "joao@email.com"
+// const senha_correta = "123456J@"
+
+// if (email.toLowerCase() === email_correto){
+//     if(senha === senha_correta){
+//         alert("Login efetuado com sucesso")
+//     }else{
+//         alert("Senha incorreta")
+//     }
+// }else{
+//     alert("Email incorreto")
+// }
+
+
+// const letra = prompt("Digite uma letra do alfabeto: ")
+
+// if (letra.length ===1){
+//     const vogais = "aeiou"
+//     const consoantes = "bcdfghjklmnpqrstuwyxz"
+
+//     if(vogais.includes(letra.toLowerCase())){
+//         alert("É uma vogal")
+//     }else if(consoantes.includes(letra.toLowerCase())){
+//         alert("É uma consoante")
+//     }else{
+//         alert("DIGITE APENAS LETRAS DO ALFABETO DE A - Z")
+//     }
+// }else{
+//     alert("Digite apenas UMA letra")
+// }
+
+
+// let contador = 0 
+
+// while (contador < 10){
+//     console.log(contador)
+//     contador ++
+// }
+
+
+
+let contador = 0 
+
+while (contador <= 50){
+    if(contador % 2 == 0)
+    console.log (contador)
+    contador ++
+}
